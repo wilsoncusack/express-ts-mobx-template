1d3ccf9d39db3ecb4856b405587ebf2b1496c5aa
Wilson Cusack <cusackw@gmail.com>
2019-04-20 21:41:25 -0400

    something working

--- dist/server.js
+++ dist/server.js
@@ -4,7 +4,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
 const PORT = 3000;
 const typeorm_1 = require("typeorm");
-typeorm_1.createConnection();
+const pg_connection_string_1 = require("pg-connection-string");
+const connectionOptions = pg_connection_string_1.parse(process.env.DATABASE_URL);
+typeorm_1.createConnection({
+    type: "postgres",
+    host: connectionOptions.host,
+    port: connectionOptions.port || 5432,
+    username: connectionOptions.user,
+    password: connectionOptions.password,
+    database: connectionOptions.database,
+    ssl: true,
+    entities: ["dist/entity/**/*.js"]
+}).catch(error => console.log(error));
 app_1.default.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
 });
--- lib/server.ts
+++ lib/server.ts
@@ -1,9 +1,22 @@
 // lib/server.ts
 import app from "./app";
 const PORT = 3000;
-import { createConnection } from 'typeorm';
+import { createConnection, ConnectionOptions } from 'typeorm';
+import {parse} from 'pg-connection-string';
 
-createConnection()
+const connectionOptions = parse(process.env.DATABASE_URL);
+
+
+createConnection(<ConnectionOptions>{
+    type: "postgres",
+    host: connectionOptions.host,
+    port: connectionOptions.port || 5432,
+    username: connectionOptions.user,
+    password: connectionOptions.password,
+    database: connectionOptions.database,
+    ssl: true,
+    entities: ["dist/entity/**/*.js"]
+}).catch(error => console.log(error));
 
 app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
